31fce91f7d08b25159f9c37c8939ce5bb4e98784
Arianne Briones <arianne.briones@kyocera.com>
2025-09-18 17:18:07 +0800

    fix contact us

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 // Update 'site' with your production URL for correct sitemap/canonical URLs.
 export default defineConfig({
-  // adapter: node({
-  //   mode: 'standalone',
-  // }),
-  // output: 'server',
+  output: 'server',
+  adapter: vercel(),
   site: 'https://progisuddigitalpartner.com',
   integrations: [
-    tailwind({
-      // Apply Tailwind to both Astro and Markdown content
-      config: { applyBaseStyles: true }
-    }),
-    sitemap()
-  ]
+    tailwind({ applyBaseStyles: true }),
+    sitemap(),
+  ],
 });
